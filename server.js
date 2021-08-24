6086ca0ffd3a822235e016fc42a9b2cb967fb5f5
zoomzoom6 <tonyhuang107@live.ca>
2021-08-24 15:22:36 -0400

    implment handlebars templating engine and create main homepage

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
+
+//set handlebars as the templating engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
